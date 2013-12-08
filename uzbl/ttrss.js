2333b0d3e8d75af25cec15d1c7d93c69c206debf
mlsimpson <mr.threv@gmail.com>
2013-12-07 19:23:19 -0500

    Added uzbl js

---
+++ uzbl/ttrss.js
@@ -0,0 +1,1 @@
+{if(confirm('Subscribe to %s in Tiny Tiny RSS?'.replace('%s',window.location.href)))window.location.href='http://threv.net/tt-rss/public.php?op=subscribe&feed_url='+window.location.href}
