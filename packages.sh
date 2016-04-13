e66ef35965d6da66d62a1af18a140c95b38d27f2
mlsimpson <mr.threv@gmail.com>
2016-04-13 10:33:10 -0400

    updated packages on mac

--- packages.sh
+++ packages.sh
@@ -1,11 +1,11 @@
 #!/usr/local/bin/zsh
 
-pip freeze | grep -v '^\-e' | cut -d = -f 1 > /Users/threv/.dotfiles/installed_pip.txt
+sudo pip freeze | grep -v '^\-e' | cut -d = -f 1 > /Users/msimpson/.dotfiles/installed_pip.txt
 
-gem list --no-versions | sed -e '/^(*|$)/d' > /Users/threv/.dotfiles/installed_gems.txt
+gem list --no-versions | sed -e '/^(*|$)/d' > /Users/msimpson/.dotfiles/installed_gems.txt
 
-brew list > /Users/threv/.dotfiles/installed_brews.txt
+brew list > /Users/msimpson/.dotfiles/installed_brews.txt
 
-perl -MExtUtils::Installed -E 'say for ExtUtils::Installed->new->modules' > /Users/threv/.dotfiles/installed_cpan.txt
+perl -MExtUtils::Installed -E 'say for ExtUtils::Installed->new->modules' > /Users/msimpson/.dotfiles/installed_cpan.txt
 
 #npm list -g --parseable | awk 'BEGIN {FS = "/" }; {print $NF}' | uniq >! installed_npms.txt
