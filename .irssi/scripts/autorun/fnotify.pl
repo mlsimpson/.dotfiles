36a6e10aeebcf04c8075916ee9981a166e08d2b1
Matt Simpson <maui@threv.net>
2018-09-12 17:32:50 -0400

    updated irssi fnotify

--- .irssi/scripts/autorun/fnotify.pl
+++ .irssi/scripts/autorun/fnotify.pl
@@ -1,81 +1,112 @@
-# todo: grap topic changes
-
 use strict;
+use warnings;
 use vars qw($VERSION %IRSSI);
-
 use Irssi;
-$VERSION = '0.0.3';
+
+$VERSION = '0.0.6';
 %IRSSI = (
-  authors     => 'Thorsten Leemhuis',
-  contact     => 'fedora@leemhuis.info',
-  name        => 'fnotify',
-  description => 'Write a notification to a file that shows who is talking to you in which channel.',
-  url         => 'http://www.leemhuis.info/files/fnotify/',
-  license     => 'GNU General Public License',
-  changed     => '$Date: 2007-01-13 12:00:00 +0100 (Sat, 13 Jan 2007) $'
+	name => 'fnotify',
+	authors => 'Tyler Abair, Thorsten Leemhuis, James Shubin' .
+               ', Serge van Ginderachter',
+	contact => 'fedora@leemhuis.info, serge@vanginderachter.be',
+	description => 'Write notifications to a file in a consistent format.',
+	license => 'GNU General Public License',
+	url => 'http://www.leemhuis.info/files/fnotify/fnotify https://ttboj.wordpress.com/',
 );
 
-#--------------------------------------------------------------------
-# In parts based on knotify.pl 0.1.1 by Hugo Haas
+#
+#	README
+#
+# To use:
+# $ cp fnotify.pl ~/.irssi/scripts/fnotify.pl
+# irssi> /load perl
+# irssi> /script load fnotify
+# irssi> /set fnotify_ignore_hilight 0 # ignore hilights of priority 0
+#
+
+#
+#	AUTHORS
+#
+# Ignore hilighted messages with priority = fnotify_ignore_hilight
+# version: 0.0.6
+# Tyler Abair <tyler.abair@gmail.com>
+#
+# Strip non-parsed left over codes (Bitlbee otr messages)
+# version: 0.0.5
+# Serge van Ginderachter <serge@vanginderachter.be>
+#
+# Consistent output formatting by James Shubin:
+# version: 0.0.4
+# https://ttboj.wordpress.com/
+# note: changed license back to original GPL from Thorsten Leemhuis (svg)
+#
+# Modified from the Thorsten Leemhuis <fedora@leemhuis.info>
+# version: 0.0.3
+# http://www.leemhuis.info/files/fnotify/fnotify
+#
+# In parts based on knotify.pl 0.1.1 by Hugo Haas:
 # http://larve.net/people/hugo/2005/01/knotify.pl
-# which is based on osd.pl 0.3.3 by Jeroen Coekaerts, Koenraad Heijlen
+#
+# Which is based on osd.pl 0.3.3 by Jeroen Coekaerts, Koenraad Heijlen:
 # http://www.irssi.org/scripts/scripts/osd.pl
 #
-# Other parts based on notify.pl from Luke Macken
+# Other parts based on notify.pl from Luke Macken:
 # http://fedora.feedjack.org/user/918/
 #
-#--------------------------------------------------------------------
 
-#--------------------------------------------------------------------
-# Private message parsing
-#--------------------------------------------------------------------
+my %config;
 
+Irssi::settings_add_int('fnotify', 'fnotify_ignore_hilight' => -1);
+$config{'ignore_hilight'} = Irssi::settings_get_int('fnotify_ignore_hilight');
+
+Irssi::signal_add(
+    'setup changed' => sub {
+        $config{'ignore_hilight'} = Irssi::settings_get_int('fnotify_ignore_hilight');
+    }
+);
+
+#
+#	catch private messages
+#
 sub priv_msg {
-  my ($server,$msg,$nick,$address,$target) = @_;
-  filewrite($nick." " .$msg );
+	my ($server, $msg, $nick, $address, $target) = @_;
+	my $msg_stripped = Irssi::strip_codes($msg);
+	my $network = $server->{tag};
+	filewrite('' . $network . ' ' . $nick . ' ' . $msg_stripped);
 }
 
-#--------------------------------------------------------------------
-# Printing hilight's
-#--------------------------------------------------------------------
-
-# sub hilight {
-#   my ($dest, $text, $stripped) = @_;
-#   if ($dest->{level} & MSGLEVEL_HILIGHT) {
-#     filewrite($dest->{target}. " " .$stripped );
-#   }
-# }
-
-#--------------------------------------------------------------------
-# Printing hilight's and notifying growl
-#--------------------------------------------------------------------
+#
+#	catch 'hilight's
+#
 sub hilight {
-  my ($dest, $text, $stripped) = @_;
-  $stripped =~ s/("|\\)/\\$1/g;
-  $stripped =~ s/("|'|`)//g;
-  $stripped =~ s/"//g;
-  if ($dest->{level} & (MSGLEVEL_HILIGHT|MSGLEVEL_MSGS)) {
-    filewrite($dest->{target}. " " .$stripped );
-    system("notify-send -t 1000 \"$stripped\"");
-  }
+	my ($dest, $text, $stripped) = @_;
+    my $ihl = $config{'ignore_hilight'};
+	if ($dest->{level} & MSGLEVEL_HILIGHT && $dest->{hilight_priority} != $ihl) {
+		my $server = $dest->{server};
+		my $network = $server->{tag};
+		filewrite($network . ' ' . $dest->{target} . ' ' . $stripped);
+	}
 }
-#--------------------------------------------------------------------
-# The actual printing
-#--------------------------------------------------------------------
 
+#
+#	write to file
+#
 sub filewrite {
-  my ($text) = @_;
-  # FIXME: there is probably a better way to get the irssi-dir...
-  open(FILE,">>$ENV{HOME}/.irssi/fnotify");
-  print FILE $text . "\n";
-  close (FILE);
+	my ($text) = @_;
+	my $fnfile = Irssi::get_irssi_dir() . "/fnotify";
+	if (!open(FILE, ">>", $fnfile)) {
+		print CLIENTCRAP "Error: cannot open $fnfile: $!";
+	} else {
+		print FILE $text . "\n";
+		if (!close(FILE)) {
+			print CLIENTCRAP "Error: cannot close $fnfile: $!";
+		}
+	}
 }
 
-#--------------------------------------------------------------------
-# Irssi::signal_add_last / Irssi::command_bind
-#--------------------------------------------------------------------
-
+#
+#	irssi signals
+#
 Irssi::signal_add_last("message private", "priv_msg");
 Irssi::signal_add_last("print text", "hilight");
 
-#- end
