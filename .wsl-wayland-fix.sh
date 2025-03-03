6c11c1b91583bfa679f8fe6563b8a517c7c207ed
Maui Threv <maui@threv.net>
2025-03-03 16:20:04 -0500

    wsl-wayland-fix

--- .wsl-wayland-fix.sh
+++ .wsl-wayland-fix.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ! -d /run/user/1000 ]; then
+if [[ ! -d /run/user/1000 ]]; then
     sudo chmod a+w /run
     sudo chmod a+w /run/user
     mkdir /run/user/1000
     ln -s /mnt/wslg/runtime-dir/wayland-0* /run/user/1000/
 fi
+
