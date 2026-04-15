39ceedf6b6bba30aabd7cc21cb55be7e206bcdca
mlsimpson <maui@threv.net>
2026-04-15 03:13:14 -0400

    removed powershell

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-function vim ($File){
-  $File = $File -replace "\\", "/"
-  bash -c "vim $File"
-}
-
-Set-PSReadLineOption -EditMode Emacs
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-Import-Module PSVirtualBox
